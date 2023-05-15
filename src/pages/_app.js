225188ede548fb790400891a06ed10896f24fdbd
Jatin Jindal <jatinjindal.2003@gmail.com>
2023-05-15 17:09:35 +0530

    I added mongoose and 2 lines to connect hahahaha

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,20 @@
-import { Provider } from 'react-redux'
-import { store } from '../app/store'
-import '../styles/globals.css'
-import '../styles/auth.css';
+import { Provider } from "react-redux";
+import { store } from "../app/store";
+import "../styles/globals.css";
+import "../styles/auth.css";
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/nextjs", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
